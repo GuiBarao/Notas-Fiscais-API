e4549de2d612c0fdce15ad390c3434e1b9fbf86c
GuiBarao <guibarao04@gmail.com>
2025-07-02 16:32:20 -0400

    Revert "merge"
    This reverts commit 46cf6a1724a18b63514b9bec2d82e27267919c3f, reversing
    changes made to f198d1773c59f891d77bc3820980325db3276bfc.

--- src/myapp/schemas/JWTSchema.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class JWTSchema(BaseModel):
-    access_token : str
-    token_type: str
--- src/myapp/utils/get_infosDB.py
+++ src/myapp/utils/get_infosDB.py
@@ -1,10 +1,6 @@
 import json
 from ..schemas.ConexaoSchema import ConexaoSchema
-<<<<<<< HEAD
 json_caminho = "src\myapp\config\conexao.json"
-=======
-json_caminho = "config\conexao.json"
->>>>>>> 244a3b9a9e4c6d95bed5108f9cabb2c1d1969be0
 
 def get_infosDB(filial:str) -> ConexaoSchema:
     with open(json_caminho, "r", encoding='utf-8') as json_read:
