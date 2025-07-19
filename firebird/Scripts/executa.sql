7aad69f866d0559a31798021b5fda7ee7151b4d5
GuiBarao <guibarao04@gmail.com>
2025-07-19 09:35:06 -0400

    refactor: Adicionando arquivo fdb e scripts SQL na raiz do projeto

---
+++ firebird/Scripts/executa.sql
@@ -0,0 +1,4 @@
+input 'C:\Users\Pax Primavera\Desktop\p\Scripts\criacao.sql';
+input 'C:\Users\Pax Primavera\Desktop\p\Scripts\insercao_cliente.sql';
+input 'C:\Users\Pax Primavera\Desktop\p\Scripts\insercao_nfse_item.sql';
+input 'C:\Users\Pax Primavera\Desktop\p\Scripts\lorem.sql';
